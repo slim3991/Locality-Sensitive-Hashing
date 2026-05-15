fde51e146e371385d3e2ccc2dcd6e29aba234d63
kegel <kegel@kth.se>
2026-05-15 11:04:41 +0200

    add todos

--- src/hash_table.hpp
+++ src/hash_table.hpp
@@ -1,25 +1,37 @@
 #pragma once
 #include "lsh.hpp"
 #include <cstddef>
-#include <deque>
 #include <functional>
-#include <list>
-#include <stdexcept>
 #include <sys/types.h>
 #include <unordered_map>
 #include <vector>
+
+// TODO:: Add multiple tables
+// TODO:: Fix the types, make it so that any iterable with a type that can be
+// cast to double is accepted
 
 namespace find_better_name {
 
 class VectorDB {
 
-  // TODO: This could be any iterable with length "data_length"
+  struct MyHash {
+    std::size_t operator()(const HashResult &s) const noexcept {
+      std::size_t seed = 0;
+      for (int i : s) {
+        // Gold ratio constant helps distribute bits
+        seed ^= std::hash<int>{}(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+      }
+      return seed;
+    }
+  };
+
+  // TODO: bdData could be any iterable with length "data_length"
   // containing anything that could be cast to a double.
   using dbData = std::vector<double>;
   using id_t = int;
 
   size_t n_buckets;   // number of bucket the LHS makes
-  size_t data_length; // lenght of the vectors
+  size_t data_length; // length of the vectors
   size_t W;
   LSH hasher;
 
@@ -35,27 +47,13 @@ public:
     table[res].push_back(ID);
   }
 
-  std::vector<id_t> querry(const dbData &data) const {
+  std::vector<id_t> query(const dbData &data) const {
     HashResult hash_res = hasher.compute_hash(data);
-    try {
-      return table.at(hash_res);
-
-    } catch (std::out_of_range) {
+    auto it = table.find(hash_res);
+    if (it != table.end())
+      return it->second;
+    else
       return {};
-    };
-  };
-
-private:
-  static struct MyHash {
-    std::size_t operator()(const HashResult &s) const noexcept {
-      std::hash<int> hasher;
-      int hash;
-      size_t n = s.size();
-      for (const auto &i : s) {
-        hash += hasher(i) / n;
-      }
-      return hash;
-    };
   };
 };
 } // namespace find_better_name
